3b3375e811c6a6c7c3ebbcbeb83615b1bc917625
ichernyshev <i_chernyshev@mail.ru>
2016-04-04 11:37:26 +0300

    Update main.js

--- javascripts/main.js
+++ javascripts/main.js
@@ -8,3 +8,14 @@ function showAgent() {
 function showHref() {
     alert(location.href);
 }
+
+function setupMessageButton(title, body) {
+  // создать
+  var messageElem = createMessage(title, body)
+  // позиционировать
+  positionMessage(messageElem)
+  // добавить обработчик на закрытие
+  addCloseOnClick(messageElem)
+  // вставить в документ
+  document.body.appendChild(messageElem)
+}
